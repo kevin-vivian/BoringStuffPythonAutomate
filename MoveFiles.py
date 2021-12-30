d3a9d2ea79397d8edf23a09e9f2a883f23aca2ab
Kevin Vivian <kevin.vivian1216@gmail.com>
2021-12-29 19:07:43 -0800

    change source directory. check in target directory if file exists

--- MoveFiles.py
+++ MoveFiles.py
@@ -10,15 +10,18 @@ def create_directory(dir):
         os.mkdir(dir)
 
 file_types = 'MOVMP4'
-source_directory = os.getcwd()
-target_directory = os.path.join('Videos')
+# source_directory = os.path.join('D:\\', 'Photos')
+source_directory = os.path.join('C:\\', 'Users', 'Kevin Vivian', 'Pictures')
+target_directory = os.path.join('D:\\', 'Videos')
+
 total_files = []
 for dirpath, dirnames, filenames in os.walk(source_directory):
     total_files += [os.path.join(dirpath, file) for file in filenames]
 
 total_files = [file for file in total_files if file.split('.')[-1] in file_types]
+
 for file in total_files:
-    if os.path.join(file, target_directory):
+    if os.path.exists(os.path.join(target_directory, file.split('\\')[-1])):
         print(f'{file} already exists in {target_directory}')
         continue
     print(f'Moving {file} to {target_directory}:\\')
